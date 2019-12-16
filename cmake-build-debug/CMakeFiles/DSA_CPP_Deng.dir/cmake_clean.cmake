70f5de90001049cd401623b5041019b9f7760d91
yhwhu <13207106053@163.com>
2019-12-16 16:49:26 +0800

    init c++ env

---
+++ cmake-build-debug/CMakeFiles/DSA_CPP_Deng.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DSA_CPP_Deng.dir/main.cpp.o"
+  "DSA_CPP_Deng"
+  "DSA_CPP_Deng.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DSA_CPP_Deng.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
